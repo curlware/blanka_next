999bda70427dc61c1db65a5c50d5defeec0c8ed1
rabius-sunny <rabiussunnyc3x@gmail.com>
2025-05-05 16:13:32 +0600

    chore: color fix

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 cursor-pointer',
+        default: 'bg-primary text-white shadow-xs hover:bg-primary/90 cursor-pointer',
         success: 'bg-emerald-600 text-white shadow-xs cursor-pointer',
         destructive:
           'cursor-pointer bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
