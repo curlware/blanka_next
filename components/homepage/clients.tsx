c1a2933dd8fcd227285a11a88a4efaaca3ae8d06
rabius-sunny <rabiussunnyc3x@gmail.com>
2025-05-05 16:11:22 +0600

    chore:fix

--- components/homepage/clients.tsx
+++ components/homepage/clients.tsx
@@ -1,29 +1,43 @@
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { SectionHeading } from "../common";
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { SectionHeading } from '../common'
 
 type TProps = {
-    data: ClientsSection | undefined
+  data: ClientsSection | undefined
 }
 
 export default function ClientSection({ data = {} }: TProps) {
-    const { title, logos, stats } = data
-    return (
-        <section
-            className={cn("relative flex justify-center items-center py-20 md:py-30  bg-secondary-foreground/15")}
-        >
-            <div className="space-y-16 mx-auto px-4 max-w-6xl container">
-                <SectionHeading title={title} />
+  const { title, logos } = data
+  return (
+    <section
+      className={cn(
+        'relative flex justify-center items-center py-20 md:py-30  bg-secondary-foreground/15'
+      )}
+    >
+      <div className='space-y-16 mx-auto px-4 max-w-6xl container'>
+        <SectionHeading title={title} />
 
-                <div className="flex flex-wrap justify-center items-center gap-4 md:gap-12">
-                    {logos?.map((logo, index) => (
-                        <Link key={index} href={logo?.link || "#"} target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border w-fit">
-                            <Image src={logo?.image?.file || ""} alt={`Client Logo ${index}`} width={180} height={65} className="" />
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </section >
-    )
+        <div className='flex flex-wrap justify-center items-center gap-4 md:gap-12'>
+          {logos?.map((logo, index) => (
+            <Link
+              key={index}
+              href={logo?.link || '#'}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='flex justify-center items-center border w-fit'
+            >
+              <Image
+                src={logo?.image?.file || ''}
+                alt={`Client Logo ${index}`}
+                width={180}
+                height={65}
+                className=''
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
